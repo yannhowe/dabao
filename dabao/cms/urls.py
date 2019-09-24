b8721e56f308b1495e1bf48acac949f7a2148260
yannhowe <yannhowe@gmail.com>
2019-09-25 04:39:28 +0800

    added forms, updated templates

--- dabao/cms/urls.py
+++ dabao/cms/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from cms.views import DashboardView, DabaoView
+from cms.views import *
 
 urlpatterns = [
-    path('', DashboardView.as_view()),
+    path('', DashboardView.as_view(), name='dashboard'),
+    path('dockerimage/add/', DockerImageCreate.as_view(), name='docker-image-add'),
+    path('dockerimage/<int:pk>/', DockerImageUpdate.as_view(), name='docker-image-update'),
+    path('dockerimage/<int:pk>/delete/', DockerImageDelete.as_view(), name='docker-image-delete'),
+    path('pivotalproduct/add/', PivotalProductCreate.as_view(), name='pivotal-product-add'),
+    path('pivotalproduct/<int:pk>/', PivotalProductUpdate.as_view(), name='pivotal-product-update'),
+    path('pivotalproduct/<int:pk>/delete/', PivotalProductDelete.as_view(), name='pivotal-product-delete'),
 ]
